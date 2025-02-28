b5735ea3bc99b9411cb5b66802cd85fd6ff99c1f
David <darevalo@gmail.com>
2025-02-27 18:37:56 -0600

    Inicio del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SafeSpaceComponent } from './components/safe-space/safe-space.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SafeSpaceComponent],
+  imports: [SafeSpaceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
